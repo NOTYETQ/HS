5b0e9f0cfb73079d05d0888e540098bc2b8cc0b3
NOTYETQ <2022156295@ub.edu.bz>
2024-05-17 18:16:33 -0600

    juts something

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,13 +1,31 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QLabel>
+#include <QDate>
+#include <QVBoxLayout>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-}
 
+    // Get the current date
+            QDate currentDate = QDate::currentDate();
+    QString dateString = currentDate.toString("dd-MM-yyyy");
+
+    // Set to label
+    ui->L_Header_DateTitle->setText("Current Date: " + dateString);
+
+    //get current time
+    QTime currentTime = QTime::currentTime();
+    QString timeString = currentTime.toString("hh:mm:ss");
+
+    //set to label
+    ui->L_Header_TimeTitle->setText("Current Time: " + timeString);
+
+}
 MainWindow::~MainWindow()
 {
     delete ui;
